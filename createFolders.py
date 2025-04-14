a595b8d616d94b11096dbb0938e5f12e0a7427af
vncnttan <vincent.tanjaya.vt@gmail.com>
2025-04-14 20:20:28 +0700

    feat: Image Snapshots based on corner cases

--- createFolders.py
+++ createFolders.py
@@ -1,8 +1,8 @@
 import os
 
 folders = {
-    "common": ["OppositeDirection", "BrakeCheck", "IgnoreRedlights", "Flood" ,"Parking", "Directional" ,"Pedestrian" ,"NightStreet"],
-    "indonesian": ["StreetPaddler", "RoadBlocks", "SmallRoads", "Deadlock", "BuswayLane", "Train"]
+    "common": ["OppositeDirection", "BrakeCheck", "IgnoreRedlights", "Flood" ,"Parking", "Directional" ,"Pedestrian" ,"NightStreet", "OneWay"],
+    "indonesian": ["StreetPaddler", "RoadBlocks", "SmallRoads", "Deadlock", "BuswayLane", "Train", "RoadSigns", "LaneTakeover"]
 }
 
 os.makedirs("Dataset", exist_ok=True)
---
+++ renameFiles.py
@@ -0,0 +1,23 @@
+import os
+
+prefix = "vincent-"
+
+folder_path = r'Dataset'
+for subfolder in os.listdir(folder_path):
+    subfolder_path = os.path.join(folder_path, subfolder)
+
+    for category in os.listdir(subfolder_path):
+        categories_path = os.path.join(subfolder_path, category)
+
+        for idx, filename in enumerate(os.listdir(categories_path)):
+            file_path = os.path.join(categories_path, filename)
+            print(file_path)
+            if os.path.isfile(file_path) and filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
+                # Create the new filename
+                new_filename = f"{prefix}{idx}{os.path.splitext(filename)[1]}"
+                new_file_path = os.path.join(categories_path, new_filename)
+                
+                # Rename the file
+                os.rename(file_path, new_file_path)
+
+print("Files renamed successfully.")
