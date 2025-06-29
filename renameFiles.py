92facac51cf8f5bbabebd20b07d4f462f2324b5e
ChronoStellar <hendrikcarlo0@gmail.com>
2025-06-29 22:07:49 +0700

    feat renamed the files to each subclass

--- renameFiles.py
+++ renameFiles.py
@@ -1,24 +1,26 @@
 import os
 
-prefix = "vincent-"
-
 folder_path = r'Dataset'
+
 for subfolder in os.listdir(folder_path):
     subfolder_path = os.path.join(folder_path, subfolder)
-
-    for category in os.listdir(subfolder_path):
-        categories_path = os.path.join(subfolder_path, category)
-
-        for idx, filename in enumerate(os.listdir(categories_path)):
-            file_path = os.path.join(categories_path, filename)
+    
+    if os.path.isdir(subfolder_path):
+        
+        for category in os.listdir(subfolder_path):
+            prefix = "".join(char for char in category if char.isupper())
+            category_path = os.path.join(subfolder_path, category)
             
-            print(file_path)
-            if os.path.isfile(file_path) and filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
-                # Create the new filename
-                new_filename = f"{prefix}{idx}{os.path.splitext(filename)[1]}"
-                new_file_path = os.path.join(categories_path, new_filename)
-                
-                # Rename the file
-                os.rename(file_path, new_file_path)
+            if os.path.isdir(category_path):
+                for idx, filename in enumerate(os.listdir(category_path), 1):
+                    file_path = os.path.join(category_path, filename)
+                    
+                    if os.path.isfile(file_path) and filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
+                        # Create the new filename with prefix and index
+                        new_filename = f"{prefix}{idx}{os.path.splitext(filename)[1]}"
+                        new_file_path = os.path.join(category_path, new_filename)
+                        
+                        # Rename the file
+                        os.rename(file_path, new_file_path)
 
 print("Files renamed successfully.")
